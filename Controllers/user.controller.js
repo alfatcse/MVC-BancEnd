a235288ab53c361dc3932e3aa5a0d1781af75950
Alfat <alfat.cse.21@gmail.com>
2023-01-15 14:22:06 +0100

    updated data to json file

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,6 +1,5 @@
-const users=require('../JSON/users.json');
-console.log(users); 
-
+const users = require("../users.json");
+const fs = require("fs");
 module.exports.getAllUsers = async (req, res, next) => {
   try {
     return res.status(200).json({ success: true, data: users });
@@ -22,9 +21,72 @@ module.exports.getRandomUser = async (req, res, next) => {
 };
 module.exports.saveUserData = async (req, res, next) => {
   try {
-    
-     console.log('req',req.body);
-     return res.status(200).json({ success: true, data: 'done' });
+    console.log("req", req.body);
+    if (
+      req.body.id &&
+      req.body.gender &&
+      req.body.name &&
+      req.body.contact &&
+      req.body.address &&
+      req.body.photoUrl
+    ) {
+      users.push(req.body);
+      fs.writeFile("users.json", JSON.stringify(users), function (err) {
+        if (err) throw err;
+      });
+      return res.status(200).json({ success: true, data: "done" });
+    } else {
+      return res
+        .status(201)
+        .json({ success: false, data: "Not update due to lack of data" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports.updateUserData = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    console.log(req.body.gender);
+    users.map((u) => {
+      if (u.id === parseInt(id)) {
+        console.log(u.name);
+        if (req.body.name !== undefined) {
+          u.name = req.body.name;
+        }
+        if (req.body.gender !== undefined) {
+          u.gender = req.body.gender;
+        }
+        if (req.body.contact !== undefined) {
+          u.contact = req.body.contact;
+        }
+        if (req.body.address !== undefined) {
+          u.address = req.body.address;
+        }
+        if (req.body.photoUrl !== undefined) {
+          u.photoUrl = req.body.photoUrl;
+        }
+        fs.writeFile("users.json", JSON.stringify(users), function (err) {
+          if (err) throw err;
+        });
+        return res
+          .status(200)
+          .json({ success: true, data: "Updated Successfully" });
+      } else {
+        return res
+          .status(200)
+          .json({ success: true, data: "Updated Successfully" });
+      }
+    });
+    return res.status(203).json({ success: false, data: "Can not update" });
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports.updateBulk = async (req, res, next) => {
+  try {
+    console.log('bulk update',req.body);
+    return res.status(200).json({ success: true, data: 'bulk update' });
   } catch (error) {
     next(error);
   }
--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -5,5 +5,6 @@ const controller = require('../../Controllers/user.controller');
 router.route('/all').get(limiter,controller.getAllUsers)
 router.route('/random').get(controller.getRandomUser)
 router.route('/save').post(controller.saveUserData)
-
+router.route('/update/:id').patch(controller.updateUserData)
+router.route('/bulkUpdate').patch(controller.updateBulk)
 module.exports=router;
